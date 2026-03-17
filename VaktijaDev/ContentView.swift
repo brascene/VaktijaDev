3723ab692b9dd7abfd4093d0150667d8771380e8
nodi <bra.scene@gmail.com>
2026-03-17 17:14:59 +0100

    Initial Commit

---
+++ VaktijaDev/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VaktijaDev
+//
+//  Created by Dino Pelic on 17. 3. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VaktijaDev/VaktijaDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaktijaDevApp.swift
+//  VaktijaDev
+//
+//  Created by Dino Pelic on 17. 3. 2026..
+//
+
+import SwiftUI
+
+@main
+struct VaktijaDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
